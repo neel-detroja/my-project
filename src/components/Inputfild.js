2f5517b24517da03c6000555fde7584959e8366b
“Neel-Detroja” <neel.detroja@shivlab.com>
2022-06-16 18:12:22 +0530

    "16/06/2022"

--- src/components/Inputfild.js
+++ src/components/Inputfild.js
@@ -2,22 +2,27 @@ import React, { useState } from "react";
 import { TextField } from "@mui/material";
 
 function Inputfild() {
-  const [data, setData] = useState([""]);
+  const [data, setData] = useState([{ data1: "", data2: "" }]);
 
   console.log("deta", data);
 
-  const handlechange = (e, i) => {
-    setData((pre) => {
+  const handlechange = (e, i, key) => {
+    setData((prestate) => {
+      prestate[i].data1 = e.target.value;
 
-      pre[i] = e.target.value;
+      // prestate[i].data1 = e.target.value;
+      // prestate[i].data2 = e.target.value;
+
+      // prestate[i] = e.target.value;
       if (e.target.value !== "") {
-        if (pre[data.length - 1] !== "") {
-          pre.push("");
+        if (prestate[data.length - 1] !== "") {
+          prestate.push({ data1: "", data2: "" });
         }
       } else {
-        pre.splice(i, 1);
+        prestate.splice(i, 1);
       }
-      return [...pre];
+
+      return [...prestate];
     });
   };
 
@@ -25,21 +30,31 @@ function Inputfild() {
     <>
       {data?.map((item, i) => {
         return (
-          <>
-            <div key={i}>
+          <div key={i}>
             <TextField
-              sx={{ left: "45%", top: "20px" }}
+              sx={{ left: "35%", top: "20px" }}
               required
+              placeholder="Enter value"
               id="filled-required"
-              label="Required"
+              // label="Required"
               variant="filled"
-              value={item}
-              onChange={(e) => handlechange(e, i)}
+              value={item.data1}
+              onChange={(e) => handlechange(e, i, "data1")}
+            />
+
+            <TextField
+              sx={{ left: "40%", top: "20px" }}
+              required
+              id="filled-require"
+              placeholder="Enter value"
+              // label="Required"
+              variant="filled"
+              value={item.data2}
+              onChange={(e) => handlechange(e, i, "data2")}
             />
             <br />
             <br />
-            </div>
-          </>
+          </div>
         );
       })}
     </>
